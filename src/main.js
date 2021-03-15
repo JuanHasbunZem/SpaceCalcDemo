d10bf7f16916af21f462044bf32fce04ef9eba24
JuanHasbunZem <zemenarwow@gmail.com>
2021-03-15 11:55:50 -0700

    add user input to create new object

--- src/main.js
+++ src/main.js
@@ -2,6 +2,12 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import SpaceCalc from './spaceCalc.js';
 
 $(document).ready(function() {
+  $('#userAge').submit(function(event) {
+    event.preventDefault();
+    const inputAge = $('#age').val();
+    const listOfAges = new SpaceCalc(inputAge, 0, 0, 0, 0, 0);
+  });
 });
