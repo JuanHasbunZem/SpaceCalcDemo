4dd3c4c8966508f90aa004e27b73fa25dc308a4c
JuanHasbunZem <zemenarwow@gmail.com>
2021-03-15 13:00:19 -0700

    use DRY to clean up age converter test and function, pass

--- __tests__/spaceCalc.test.js
+++ __tests__/spaceCalc.test.js
@@ -4,27 +4,25 @@ describe('SpaceCalc', () => {
   let reusableCalc;
 
   beforeEach(() => {
-    reusableCalc = new SpaceCalc(7,3,1,0,5,78);
+    reusableCalc = new SpaceCalc(7,1,2,3,4,5,78);
   });
 
   test('should correctly create a SpaceCalc object', () => {
     expect(reusableCalc.userAge).toEqual(7);
-    expect(reusableCalc.earth).toEqual(3);
-    expect(reusableCalc.mercury).toEqual(1);
-    expect(reusableCalc.venus).toEqual(0);
+    expect(reusableCalc.earth).toEqual(1);
+    expect(reusableCalc.mercury).toEqual(2);
+    expect(reusableCalc.venus).toEqual(3);
+    expect(reusableCalc.mars).toEqual(4);
     expect(reusableCalc.jupiter).toEqual(5);
     expect(reusableCalc.avgLife).toEqual(78);
   });
 
-  test('should convert userAge from years to days', () => {
-    expect(reusableCalc.convertAge()).toEqual(2555);
-  });
-
-  test('should convert userAge from earth years to mercury years', () => {
-    expect(reusableCalc.mercYears()).toEqual(2);
-  });
-
-  test('should convert userAge from earth years to venus years', () => {
-    expect(reusableCalc.venusYears()).toEqual(4);
+  test('should convert userAge from earth to cooresponding planets years', () => {
+    reusableCalc.convertAge();
+    expect(reusableCalc.earth).toEqual(7);
+    expect(reusableCalc.mercury).toEqual(29);
+    expect(reusableCalc.venus).toEqual(11);
+    expect(reusableCalc.mars).toEqual(4);
+    expect(reusableCalc.jupiter).toEqual(1)
   });
 });
--- src/spaceCalc.js
+++ src/spaceCalc.js
@@ -1,27 +1,20 @@
 export default class SpaceCalc {
-  constructor(userAge, earth, mercury, venus, jupiter, avgLife) {
+  constructor(userAge, earth, mercury, venus, mars, jupiter, avgLife) {
     this.userAge = userAge;
     this.earth = earth;
     this.mercury = mercury;
     this.venus = venus;
+    this.mars = mars;
     this.jupiter = jupiter;
     this.avgLife = avgLife;
   }
 
   convertAge() {
-    this.userAge = this.userAge * 365;
-    return this.userAge;
+    this.earth = Math.round(this.userAge / 1.00);
+    this.mercury = Math.round(this.userAge / 0.24);
+    this.venus = Math.round(this.userAge / 0.62);
+    this.mars = Math.round(this.userAge / 1.88);
+    this.jupiter = Math.round(this.userAge / 11.86);
   }
-
-  mercYears() {
-    this.mercury = Math.round(this.userAge * 0.24);
-    return this.mercury;
-  }
-
-  venusYears() {
-    this.venus = Math.round(this.userAge * 0.62);
-    return this.venus;
-  }
-  
 
 }
