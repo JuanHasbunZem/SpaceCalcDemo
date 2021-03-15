d0dd571420e25701222ee905426703adf8d06b7b
JuanHasbunZem <zemenarwow@gmail.com>
2021-03-15 12:20:22 -0700

    test if object construct is built, pass

---
+++ __tests__/spaceCalc.test.js
@@ -0,0 +1,18 @@
+import SpaceCalc from './../src/spaceCalc.js';
+
+describe('SpaceCalc', () => {
+  let reusableCalc;
+
+  beforeEach(() => {
+    reusableCalc = new SpaceCalc(7,3,1,0,5,78);
+  });
+
+  test('should correctly create a SpaceCalc object', () => {
+    expect(reusableCalc.userAge).toEqual(7);
+    expect(reusableCalc.earth).toEqual(3);
+    expect(reusableCalc.mercury).toEqual(1);
+    expect(reusableCalc.venus).toEqual(0);
+    expect(reusableCalc.jupiter).toEqual(5);
+    expect(reusableCalc.avgLife).toEqual(78);
+  });
+});
