f8e80d44a2870e2b4198b90d91d21f45e39faba2
JuanHasbunZem <zemenarwow@gmail.com>
2021-03-15 11:43:20 -0700

    add constructor

---
+++ src/spaceCalc.js
@@ -0,0 +1,10 @@
+export default class SpaceCalc {
+  constructor(userAge, earth, mercury, venus, jupiter, avgLife) {
+    this.userAge = userAge;
+    this.earth = earth;
+    this.mercury = mercury;
+    this.venus = venus;
+    this.jupiter = jupiter;
+    this.avgLife = avgLife;
+  }
+}
