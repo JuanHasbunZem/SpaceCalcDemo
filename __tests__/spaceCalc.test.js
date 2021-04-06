d4204b8f30dfaafc4a626bad1b0fcfe1e7a9eed9
JuanHasbunZem <zemenarwow@gmail.com>
2021-04-06 15:25:41 -0700

    update spaceCalc tests with new Object mods, failed version

--- __tests__/spaceCalc.test.js
+++ __tests__/spaceCalc.test.js
@@ -4,24 +4,10 @@ describe('SpaceCalc', () => {
   let reusableCalc;
 
   beforeEach(() => {
-    reusableCalc = new SpaceCalc(7,1,2,3,4,5);
+    reusableCalc = new SpaceCalc(7);
   });
 
   test('should correctly create a SpaceCalc object', () => {
-    expect(reusableCalc.userAge).toEqual(7);
-    expect(reusableCalc.earth).toEqual(1);
-    expect(reusableCalc.mercury).toEqual(2);
-    expect(reusableCalc.venus).toEqual(3);
-    expect(reusableCalc.mars).toEqual(4);
-    expect(reusableCalc.jupiter).toEqual(5);
-  });
-
-  test('should convert userAge from earth to cooresponding planets years', () => {
-    reusableCalc.convertAge();
-    expect(reusableCalc.earth).toEqual(7);
-    expect(reusableCalc.mercury).toEqual(29);
-    expect(reusableCalc.venus).toEqual(11);
-    expect(reusableCalc.mars).toEqual(4);
-    expect(reusableCalc.jupiter).toEqual(1);
+    expect(reusableCalc.userAge).toEqual(1);
   });
 });
