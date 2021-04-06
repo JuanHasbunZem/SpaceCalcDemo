d1b27cbb88c35e97aaa0fee48728e7b8d16c5414
JuanHasbunZem <zemenarwow@gmail.com>
2021-04-06 15:28:15 -0700

    2nd spaceCalc test failed

--- __tests__/spaceCalc.test.js
+++ __tests__/spaceCalc.test.js
@@ -10,4 +10,8 @@ describe('SpaceCalc', () => {
   test('should correctly create a SpaceCalc object', () => {
     expect(reusableCalc.userAge).toEqual(7);
   });
+
+  test('should correctly calculate your age on Mercury', () => {
+    expect(reusableCalc.mercury).toEqual(7);
+  });
 });
