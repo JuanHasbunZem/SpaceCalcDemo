4e5641bf516c3deb667517de3183ed0cc5da32cb
JuanHasbunZem <zemenarwow@gmail.com>
2021-04-06 15:41:16 -0700

    lifeAve test 1, failed

--- __tests__/lifeAve.test.js
+++ __tests__/lifeAve.test.js
@@ -4,24 +4,10 @@ describe('LifeAverage', () => {
   let reusableObj;
 
   beforeEach(() => {
-    reusableObj = new LifeAverage(78, 1, 2, 3, 4, 5);
+    reusableObj = new LifeAverage(78);
   });
 
   test('should correctly create a LifeAverage object', () => {
-    expect(reusableObj.lifeAvg).toEqual(78);
-    expect(reusableObj.earthAvg).toEqual(1);
-    expect(reusableObj.mercuryAvg).toEqual(2);
-    expect(reusableObj.venusAvg).toEqual(3);
-    expect(reusableObj.marsAvg).toEqual(4);
-    expect(reusableObj.jupiterAvg).toEqual(5);
-  });
-
-  test('should correctly create convert life averages for each planet', () => {
-    reusableObj.convertLifeAvgs();
-    expect(reusableObj.earthAvg).toEqual(78);
-    expect(reusableObj.mercuryAvg).toEqual(325);
-    expect(reusableObj.venusAvg).toEqual(126);
-    expect(reusableObj.marsAvg).toEqual(41);
-    expect(reusableObj.jupiterAvg).toEqual(7);
+    expect(reusableObj.lifeAvg).toEqual(1);
   });
 });
