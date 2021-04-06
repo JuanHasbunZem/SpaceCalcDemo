bcc14c9438452c3d9203e91584c51fea2e618163
JuanHasbunZem <zemenarwow@gmail.com>
2021-04-06 15:23:45 -0700

    change spaceCalc object to do all calcs for ages at once, also ran failed test

--- src/spaceCalc.js
+++ src/spaceCalc.js
@@ -1,19 +1,10 @@
 export default class SpaceCalc {
-  constructor(userAge, earth, mercury, venus, mars, jupiter) {
+  constructor(userAge) {
     this.userAge = userAge;
-    this.earth = earth;
-    this.mercury = mercury;
-    this.venus = venus;
-    this.mars = mars;
-    this.jupiter = jupiter;
-  }
-
-  convertAge() {
     this.earth = Math.round(this.userAge / 1.00);
     this.mercury = Math.round(this.userAge / 0.24);
     this.venus = Math.round(this.userAge / 0.62);
     this.mars = Math.round(this.userAge / 1.88);
     this.jupiter = Math.round(this.userAge / 11.86);
   }
-
 }
