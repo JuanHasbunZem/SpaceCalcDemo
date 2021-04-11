16903a77d1ec97f0f471fa45514dd44b6b4e603b
JuanHasbunZem <zemenarwow@gmail.com>
2021-04-10 21:18:05 -0700

    add if/else loops to determine life expectancy

--- src/main.js
+++ src/main.js
@@ -24,7 +24,35 @@ $(document).ready(function() {
     $("#marAvg").text(`${lifeAverages.marsAvg}`);
     $("#jupAvg").text(`${lifeAverages.jupiterAvg}`);
 
-    checkLife(inputAge);
+    if(78-inputAge <= 0) {
+      $("#lifeExpectEarth").text(`Wow! You've exceeded the life expectancy on Earth by ${(78-inputAge) * -1} year(s)!`);
+    } else {
+      $("#lifeExpectEarth").text(`On Earth you have an estimated ${78-inputAge} year(s) left!`);
+    }
+
+    if(lifeAverages.mercuryAvg - listOfAges.mercury <= 0) {
+      $("#lifeExpectMercury").text(`Wow! You've exceeded the life expectancy on Mercury by ${(lifeAverages.mercuryAvg - listOfAges.mercury) * -1} year(s)!`);
+    } else {
+      $("#lifeExpectMercury").text(`On Mercury you have an estimated ${lifeAverages.mercuryAvg - listOfAges.mercury} year(s) left!`);
+    }
+
+    if(lifeAverages.venusAvg - listOfAges.venus <= 0) {
+      $("#lifeExpectVenus").text(`Wow! You've exceeded the life expectancy on Venus by ${(lifeAverages.venusAvg - listOfAges.venus) * -1} year(s)!`);
+    } else {
+      $("#lifeExpectVenus").text(`On Venus you have an estimated ${lifeAverages.venusAvg - listOfAges.venus} year(s) left!`);
+    }
+
+    if(lifeAverages.marsAvg - listOfAges.mars <= 0) {
+      $("#lifeExpectMars").text(`Wow! You've exceeded the life expectancy on Mars by ${(lifeAverages.marsAvg - listOfAges.mars) * -1} year(s)!`);
+    } else {
+      $("#lifeExpectMars").text(`On Mars you have an estimated ${lifeAverages.marsAvg - listOfAges.mars} year(s) left!`);
+    }
+
+    if(lifeAverages.jupiterAvg - listOfAges.jupiter <= 0) {
+      $("#lifeExpectJupi").text(`Wow! You've exceeded the life expectancy on Jupiter by ${(lifeAverages.jupiterAvg - listOfAges.jupiter) * -1} year(s)!`);
+    } else {
+      $("#lifeExpectJupi").text(`On Jupiter you have an estimated ${lifeAverages.jupiterAvg - listOfAges.jupiter} year(s) left!`);
+    }
 
   });
 });
